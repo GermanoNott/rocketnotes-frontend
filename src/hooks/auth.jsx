f4749d9596d9963c7b2565922b56386269aaf10c
Matheus <matheus-rodrigues37@live.com>
2022-10-11 12:11:52 -0300

    problema estava no componente signin

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -1,33 +1,31 @@
-import { createContext, useContext } from 'react';
-import { api } from '../services/api';
+import { createContext, useContext } from "react";
+import { api } from "../services/api";
 
 export const AuthContext = createContext({});
 
 function AuthProvider({ children }) {
-    async function signIn({ email, password }) {
-        try {
-            const response = await api.post('/sessions', { email, password });
-            console.log(response);
-        } catch (error) {
-            if (error.response) {
-                alert(error.response.data.message);
-            } else {
-                alert('Não foi possível entrar');
-            }
-        }
+  async function signIn({ email, password }) {
+    try {
+      const response = await api.post("/sessions", { email, password });
+      console.log(response);
+    } catch (error) {
+      if (error.response) {
+        alert(error.response.data.message);
+      } else {
+        alert("Não foi possível entrar");
+      }
     }
+  }
 
-    return (
-        <AuthContext.Provider value={{ signIn }}>
-            {children}
-        </AuthContext.Provider>
-    );
+  return (
+    <AuthContext.Provider value={{ signIn }}>{children}</AuthContext.Provider>
+  );
 }
 
 function useAuth() {
-    const context = useContext(AuthContext);
+  const context = useContext(AuthContext);
 
-    return context;
+  return context;
 }
 
 export { AuthProvider, useAuth };
--- src/pages/Signin/index.jsx
+++ src/pages/Signin/index.jsx
@@ -1,49 +1,48 @@
-import { useState } from 'react';
-import { FiMail, FiLock } from 'react-icons/fi';
-import { Link } from 'react-router-dom';
-import { Input } from '../../components/Input';
-import { Button } from '../../components/Button';
-import { useAuth } from '../../hooks/auth';
+import { useState } from "react";
+import { FiMail, FiLock } from "react-icons/fi";
+import { Link } from "react-router-dom";
+import { Input } from "../../components/Input";
+import { Button } from "../../components/Button";
+import { useAuth } from "../../hooks/auth";
 
-import { Container, Form, Background } from './styles';
+import { Container, Form, Background } from "./styles";
 
 export function SignIn() {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const { signIn } = useAuth();
+  const { signIn } = useAuth();
 
-    function handleSignIn() {
-        signIn({ email, password });
-    }
-    return (
-        <Container>
-            <Form>
-                <h1>Rocket Notes</h1>
-                <p>Aplicação para salvar e gerenciar seus links uteis.</p>
+  function handleSignIn() {
+    signIn({ email, password });
+  }
+  return (
+    <Container>
+      <Form>
+        <h1>Rocket Notes</h1>
+        <p>Aplicação para salvar e gerenciar seus links uteis.</p>
 
-                <h2>Faça seu login</h2>
-                <Input
-                    placeholder="E-mail"
-                    type="text"
-                    icon={FiMail}
-                    onChange={e => setEmail(e.target.valeu)}
-                />
-                <Input
-                    placeholder="Senha"
-                    type="password"
-                    icon={FiLock}
-                    onChange={e => setPassword(e.target.valeu)}
-                />
+        <h2>Faça seu login</h2>
+        <Input
+          placeholder="E-mail"
+          type="text"
+          icon={FiMail}
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+        <Input
+          placeholder="Senha"
+          type="password"
+          icon={FiLock}
+          onChange={(e) => setPassword(e.target.value)}
+          value={email}
+        />
 
-                <Button
-                    title="Entrar"
-                    onClick={handleSignIn}
-                ></Button>
+        <Button title="Entrar" onClick={handleSignIn}></Button>
 
-                <Link to="/register">Criar conta</Link>
-            </Form>
-            <Background />
-        </Container>
-    );
+        <Link to="/register">Criar conta</Link>
+      </Form>
+      <Background />
+    </Container>
+  );
 }
